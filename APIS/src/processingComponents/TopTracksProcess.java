698b5d5a5d87aa5ba97903016f0260968d7f0275
jmgdtfg <34899529+jmgdtfg@users.noreply.github.com>
2018-04-15 17:36:22 +0200

    Rename TopPlaylistTracksProcess.java to TopTracksProcess.java

--- APIS/src/processingComponents/TopTracksProcess.java
+++ APIS/src/processingComponents/TopTracksProcess.java
@@ -12,21 +12,21 @@ import com.wrapper.spotify.model_objects.specification.PlaylistTrack;
 
 public class TopPlaylistTracksProcess implements ProcessComponent{
 	
-	private int numTop_; //numero de valores que devolver· el top
+	private int numTop_; //numero de valores que devolver√° el top
 	
 	//Constructor parametrizado obligatorio de la clase
 	public TopPlaylistTracksProcess(int numTop){
 		numTop_=numTop;
 	}
 	
-	//FunciÛn que devuelve las canciones m·s escuchadas de una playlist
+	//Funci√≥n que devuelve las canciones m√°s escuchadas de una playlist
 	@Override
 	public Object execute(Object data, Map<String, String> configuration) {
 		
 		Playlist p = (Playlist) data;
 		Paging<PlaylistTrack> t = p.getTracks();
 		PlaylistTrack[] tracks = t.getItems();		
-		//Lista de String. Cada elemento contiene la informaciÛn de una canciÛn
+		//Lista de String. Cada elemento contiene la informaci√≥n de una canci√≥n
 		List<String> listTracks = new ArrayList<String>();
 		String artists = "Artista(s): ";
 		
@@ -39,24 +39,24 @@ public class TopPlaylistTracksProcess implements ProcessComponent{
 		}
 		//Se obtiene la lista de tracks ordenada
 		for (PlaylistTrack pt : trackList){
-			//Si el elemento es m·s popular lo aÒade al principio
+			//Si el elemento es m√°s popular lo a√±ade al principio
 			if (pt.getTrack().getPopularity() > trackList.get(0).getTrack().getPopularity()){
 				sortTracks.add(0, pt);
 			}
 		}
-		//Recorre la lista ordenada de tracks recogiendo la informaciÛn necesaria
+		//Recorre la lista ordenada de tracks recogiendo la informaci√≥n necesaria
 		for (PlaylistTrack track : sortTracks){
 			//Obtiene el artista(s)
 			ArtistSimplified[] at = track.getTrack().getArtists();
 			for (ArtistSimplified a : at){
 				artists = artists.concat(a.getName()+" ");
 			}
-			//Concatena a los artistas el tÌtulo de la canciÛn
-			listTracks.add(artists+" | TÌtulo: "+track.getTrack().getName());
+			//Concatena a los artistas el t√≠tulo de la canci√≥n
+			listTracks.add(artists+" | T√≠tulo: "+track.getTrack().getName());
 			artists = "Artista(s): ";
 		}
 		
-		//ElaboraciÛn del mensaje
+		//Elaboraci√≥n del mensaje
 		String message = "Top canciones: \n\n";
 		for (int i = 0; i < numTop_ ; i++){
 			message = message.concat("\n"+listTracks.get(i));
